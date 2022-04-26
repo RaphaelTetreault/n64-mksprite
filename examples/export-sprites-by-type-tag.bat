2dc6c716aa5f332d37ccccca038a15e3a2714f70
Raphaël Tétreault <raphaeltetreault@outlook.com>
2022-04-25 23:30:26 -0400

    m: boolean arguments don't need an explicit call for 'true' when default is 'false'

--- examples/export-sprites-by-type-tag.bat
+++ examples/export-sprites-by-type-tag.bat
@@ -13,11 +13,11 @@ set output=".\filesystem"
 :: 7 - [-f <format>]		The sprite format to convert to. [rgba32|rgba16|ci8|ci4|i8|i4|ia8|ia4]
 :: 8 - [--rmExts <bool>]	Remove all extensions on processed files. Set on here since this example tags images with format extension.
 
-%mksprite% -m 1 -i %input% -o %output% -f rgba32 --searchSubdirs=true --searchPattern="*.rgba32*.png" --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f rgba16 --searchSubdirs=true --searchPattern="*.rgba16*.png" --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f ci8    --searchSubdirs=true --searchPattern="*.ci8*.png"    --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f i8     --searchSubdirs=true --searchPattern="*.i8*.png"     --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f ia8    --searchSubdirs=true --searchPattern="*.ia8*.png"    --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f ci4    --searchSubdirs=true --searchPattern="*.ci4*.png"    --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f i4     --searchSubdirs=true --searchPattern="*.i4*.png"     --rmExts=true
-%mksprite% -m 1 -i %input% -o %output% -f ia4    --searchSubdirs=true --searchPattern="*.ia4*.png"    --rmExts=true
+%mksprite% -m 1 -i %input% -o %output% -f rgba32 --searchSubdirs --searchPattern="*.rgba32*.png" --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f rgba16 --searchSubdirs --searchPattern="*.rgba16*.png" --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f ci8    --searchSubdirs --searchPattern="*.ci8*.png"    --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f i8     --searchSubdirs --searchPattern="*.i8*.png"     --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f ia8    --searchSubdirs --searchPattern="*.ia8*.png"    --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f ci4    --searchSubdirs --searchPattern="*.ci4*.png"    --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f i4     --searchSubdirs --searchPattern="*.i4*.png"     --rmExts
+%mksprite% -m 1 -i %input% -o %output% -f ia4    --searchSubdirs --searchPattern="*.ia4*.png"    --rmExts
